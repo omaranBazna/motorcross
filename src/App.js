d6843f7bb004b67e2b4266f3a1e825da7819461f
Omaran Bazna <omran4d@gmail.com>
2022-05-16 15:33:15 -0400

    first version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
+import logo from './graphics/logo.png'
+import image1 from './graphics/thumps/1.JPG'
+import image2 from './graphics/thumps/2.JPG'
+import image3 from './graphics/thumps/3.JPG'
+import image4 from './graphics/thumps/4.JPG'
+import image5 from './graphics/thumps/5.JPG'
+import image6 from './graphics/thumps/6.JPG'
 import './App.css';
 
+const images = [image1,image2,image3,image4,image5,image6];
 function App() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+      const intervalId = setInterval(() => {
+          if(currentIndex === images.length - 1) {
+              setCurrentIndex(0);
+          } 
+          else {
+               setCurrentIndex(currentIndex + 1);
+          }
+      }, 5000)
+      
+      return () => clearInterval(intervalId);
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    
+    <nav>
+      <img src={logo} width="60" height="30"/>
+      <div>
+        <a herf="#">
+        AbuDubiMotor
         </a>
-      </header>
-    </div>
+        <a href="#">
+         tutors
+        </a>
+        <a href="#">
+         tracks
+        </a>
+        <a href="#">
+        Results
+        </a>
+      </div>
+ 
+
+    </nav>
+    <section className='hero'>
+      <div className='left'>
+         <h1>
+           Motocross Club in Abu Dhabi
+         </h1>
+          <h2>
+         “Great memories happen when you don’t know where you’re going”
+         </h2>
+      </div>
+      <div className='right'>
+     
+        
+    
+       <img src={currentImage} height="450px" ></img>
+      </div>
+      
+    </section>
+    <section className='tutor'>
+
+    </section>
+    <section className='calender'>
+
+    </section>
+    <section className='results'>
+
+    </section>
+    <footer>
+
+    </footer>
+     </div>
   );
 }
 
