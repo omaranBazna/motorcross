fe085aa27a84118f8ac1166f6da748a29e4ed30f
Omaran Bazna <omran4d@gmail.com>
2022-05-17 13:54:49 -0400

    hero section attemp 1 completed

--- src/App.js
+++ src/App.js
@@ -4,35 +4,14 @@ import image2 from './graphics/thumps/2.JPG'
 import image3 from './graphics/thumps/3.JPG'
 import image5 from './graphics/thumps/5.JPG'
 import image6 from './graphics/thumps/6.JPG'
-import React, { useState,useEffect } from "react";
+import React from "react";
 
 import './App.css';
-
-const images = [image1,image2,image3,image5,image6];
-const names=["start","middle","middle","middle","middle","middle","middle","middle","middle","middle","middle","middle","end"]
 export default function App() {
-  const [currentImage, setCurrentImage] = useState(0);
-  const [name,setName]=useState("start");
-  let i=0;
-  let u=0;
-useEffect(() => {
-  const interval = setInterval(() => {
-    i +=1;
-    setCurrentImage(i%5);
-
-  }, 3000);
-  return () => clearInterval(interval);
-}, []);
 
 
-useEffect(() => {
-  const interval = setInterval(() => {
-    u +=1;
-    setName(names[u%13]);
-    
-  }, 3000/13);
-  return () => clearInterval(interval);
-}, []);
+
+
 
   return (
     <div className="App">
@@ -67,7 +46,7 @@ useEffect(() => {
       </div>
       <div className='right'>
       
-          <img src={images[currentImage]} className={name} height="450px" ></img>
+          <img src={image1}  height="450px" ></img>
     
       
       </div>
